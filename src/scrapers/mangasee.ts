45dcf1255a73ee8a232dd66be0afffaa5ee116e5
Jip Fr <jipfrijlink@gmail.com>
2020-08-01 22:12:30 +0200

    Fix typo

--- src/scrapers/mangasee.ts
+++ src/scrapers/mangasee.ts
@@ -178,7 +178,7 @@ class MangaseeClass {
 			}
 		} catch(err) {
 			//  OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!
-			console.err(err.stack);
+			console.error(err.stack);
 			return error(-1, err);
 		}
 
