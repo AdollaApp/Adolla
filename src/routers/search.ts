6c1d5c47302a9df5518bf4fc25d9d32fedd9e5cf
Jip Fr <jipfrijlink@gmail.com>
2020-08-02 16:23:55 +0200

    Add reading to search sidebar, add placeholder logo, change quick-select text

--- src/routers/search.ts
+++ src/routers/search.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 
 import Mangasee from "../scrapers/mangasee";
 import { setMangaProgress } from "../util/getMangaProgress";
+import getReading from "../util/getReading";
 
 router.get("/", async (req, res) => {
 	let query = req.query.q as string;
@@ -12,7 +13,10 @@ router.get("/", async (req, res) => {
 	
 	await Promise.all(searchResults.map(setMangaProgress));
 
+	let reading = await getReading();
+
 	res.render("search", {
+		reading,
 		query,
 		searchResults
 	});
