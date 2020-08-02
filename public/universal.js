ed8b3964115ce920ade9f2007993bd6cb0486510
Jip Fr <jipfrijlink@gmail.com>
2020-08-02 13:34:59 +0200

    Update quick select

--- public/universal.js
+++ public/universal.js
@@ -11,7 +11,10 @@ document.querySelectorAll(".toggle-quick-select").forEach(div => {
 	div.addEventListener("click", evt => {
 		if(!evt.composedPath().includes(document.querySelector(".quick-select"))) {
 			document.querySelector("html").classList.toggle("overlay-visible")
-			document.querySelector(".quick-select-wrapper").classList.toggle("visible")
+			document.querySelector(".quick-select-wrapper").classList.toggle("visible");
+			document.querySelector(".quick-select-wrapper .scroll .current-chapter").scrollIntoView({
+				block: "center"
+			});
 		};
 	})
 });
@@ -22,21 +25,3 @@ document.querySelectorAll(".chapterLink").forEach(link => {
 		link.classList.add("clicked", "badge-background");
 	});
 });
-
-
-// Chapter quick select scroll
-// document.querySelectorAll(".scroll").forEach(wrapper => {
-// 	updateScroll(wrapper);
-// 	wrapper.addEventListener("scroll", () => {
-// 		updateScroll(wrapper);
-// 	});
-// });
-
-// function updateScroll(wrapper) {
-// 	let wrapperTop = wrapper.getBoundingClientRect().top;
-// 	wrapper.querySelectorAll(".chapter").forEach(div => {
-// 		let top = (div.getBoundingClientRect().top - wrapperTop);
-// 		let middleOffset = (top - wrapper.offsetHeight / 2) + (div.scrollHeight /2);
-// 		div.style.opacity = 1 - Math.abs(middleOffset / 160);
-// 	});
-// }
