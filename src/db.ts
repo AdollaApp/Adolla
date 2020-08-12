549fa081585d30a6b7d9042e4b367c8791191919
Jip Fr <jipfrijlink@gmail.com>
2020-08-12 18:34:05 +0200

    Switch over to external module for db

--- src/db.ts
+++ src/db.ts
@@ -1,62 +1,15 @@
 
 import { Database } from "./types";
-import fs from "fs";
+import Db from "jipdb";
 
-class Db {
-	
-	private db: Db;
-	private file: string;
-	public data: any;
-	private storeDebounce: any;
-
-	constructor() {
-		this.db = this;
-
-		this.file = `./data.json`;
-
-		if(!fs.existsSync(this.file)) fs.writeFileSync(this.file, "{}");
-		this.data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
-
-		const defaults: Database = {
-			manga_cache: {},
-			reading: {},
-			other: {},
-			notified: {},
-			lists: []
-		}
-		for(let key of Object.keys(defaults)) {
-			if(!this.data[key]) this.data[key] = defaults[key];
-		}
-
-		this.store();
-
-	}
-
-	public store(doDebounce = true) {
-		if(this.storeDebounce) {
-			clearTimeout(this.storeDebounce);
-			delete this.storeDebounce;
-		}
-		this.storeDebounce = setTimeout(() => {
-			fs.writeFileSync(this.file, JSON.stringify(this.data));
-		}, doDebounce ? 15e3 : 0);	
-	}
-
-	public get(str: string, setFields = false) {
-		let value = str.split(".").reduce((p, c)=> {
-			if(p && !p[c] && setFields) p[c] = {};
-			return (p && p[c]) ?? null;
-		}, this.data);
-		return value;
-	}
-
-	public set(str: string, value: any) {
-		let field = this.get(str.split(".").slice(0, -1).join("."), true);
-		field[str.split(".").pop()] = value;
-		// field[] = value;
-	}
+const defaults: Database = {
+	manga_cache: {},
+	reading: {},
+	other: {},
+	notified: {},
+	lists: []
 }
 
-const db = new Db();
+const db = new Db("data.json", defaults);
 
 export default db;
