bdf0e87963fec54d92aa17a2c645eba255251a1b
Jip Fr <jipfrijlink@gmail.com>
2020-08-20 17:47:21 +0200

    Add "creator's recommendations" and store lists open state

--- src/routers/lists.ts
+++ src/routers/lists.ts
@@ -1,12 +1,32 @@
 
+import chalk from "chalk";
+import fetch from "node-fetch";
+
 import express from "express";
 const router = express.Router();
 
 import getReading from "../util/getReading";
 import updateManga from "../util/updateManga";
 
+import db from "../db";
 
-const lists = [
+// Get "recommended" list
+let recommendedLists = [];
+async function updateRecommended() {
+	console.info(chalk.yellowBright("[RECOMMENDATIONS]") + ` Updating recommendations at ${new Date().toLocaleString()}`);
+
+	const suggestionsUrl = "https://gist.githubusercontent.com/JipFr/17fabda0f0515965cbe1c73b75b7ed71/raw";
+	let recommended = await (await fetch(suggestionsUrl)).json();
+	recommendedLists = recommended;
+
+	console.info(chalk.green("[RECOMMENDATIONS]") + ` Updated recommendations`);
+
+}
+updateRecommended();
+setInterval(updateRecommended, 1e3 * 60 * 60 * 12); // Update every 12 hours
+
+// Set default lists. This is temporary
+db.set("lists", [
 	{
 		"slug": "good",
 		"name": "Good",
@@ -48,18 +68,21 @@ const lists = [
 		"name": "This is an empty lists",
 		"entries": []
 	}
-];
+]);
 
 
 router.get("/", async (req, res) => {
 	
 	// Set lists
-	let updatedLists = Object.assign([], lists);
+	let lists = db.get("lists");
+	let updatedLists = Object.assign([], [...recommendedLists, ...lists]);
 	for(let list of updatedLists) {
+
 		// Add data for each item
 		for(let entry of list.entries) {
 			entry.data = await updateManga(entry.slug);
 		}
+		
 	}
 	
 	// Get reading 
