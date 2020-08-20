1995a38547f78e05fe20dfc052702ece3158fdf9
Jip Fr <jipfrijlink@gmail.com>
2020-08-20 16:51:42 +0200

    Get started on lists (again)

---
+++ src/routers/lists.ts
@@ -0,0 +1,70 @@
+
+import express from "express";
+const router = express.Router();
+
+import getReading from "../util/getReading";
+import updateManga from "../util/updateManga";
+
+
+const lists = [
+	{
+		"slug": "good",
+		"name": "Good",
+		"entries": [
+			{
+				"slug": "Fire-Brigade-Of-Flames"
+			},
+			{
+				"slug": "Tower-Of-God"
+			},
+			{
+				"slug": "Shingeki-No-Kyojin"
+			},
+			{
+				"slug": "Yokohama-Kaidashi-Kikou"
+			}
+		]
+	},
+	{
+		"slug": "bad",
+		"name": "Kinda lame",
+		"entries": [
+			{
+				"slug": "Dr-Stone"
+			}
+		]
+	},
+	{
+		"slug": "might-read",
+		"name": "Might read",
+		"entries": [
+			{
+				"slug": "The-God-Of-High-School"
+			}
+		]
+	}
+];
+
+
+router.get("/", async (req, res) => {
+	
+	// Set lists
+	let updatedLists = Object.assign([], lists);
+	for(let list of updatedLists) {
+		// Add data for each item
+		for(let entry of list.entries) {
+			entry.data = await updateManga(entry.slug);
+		}
+	}
+	
+	// Get reading 
+	let reading = await getReading();
+
+	res.render("lists", {
+		reading,
+		lists: updatedLists,
+		isLists: true
+	});
+});
+
+export default router;
