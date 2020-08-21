4b767b47a0a764add3c087d2f8a9cf6b6a0fd802
Jip Fr <jipfrijlink@gmail.com>
2020-08-21 15:29:37 +0200

    Change some styling on manga page

--- public/js/lists-editor.js
+++ public/js/lists-editor.js
@@ -9,7 +9,7 @@ function updateLists() {
 		let span = document.createElement("span");
 
 		// Add classes
-		span.classList.add("badge", "badge-background");
+		span.classList.add("secondary-badge");
 
 		// Set content
 		span.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></svg>`;
@@ -17,7 +17,6 @@ function updateLists() {
 
 		// Add "remove from list" functionality
 		span.querySelector("svg").addEventListener("click", () => {
-			console.log(lists[0].slug, list.slug);
 			lists = lists.filter(l => l.slug !== list.slug);
 			updateLists();
 			setLists();
