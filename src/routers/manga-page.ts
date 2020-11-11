5eabdbd0b45e9cf35bd5da296459e084eb70b958
Jip Fr <jipfrijlink@gmail.com>
2020-11-11 21:35:12 +0100

    Remove reading type

--- src/routers/manga-page.ts
+++ src/routers/manga-page.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 import db from "../db";
 import updateManga from "../util/updateManga";
-import { Progress, StoredData, List, Reading } from "../types";
+import { Progress, StoredData, List } from "../types";
 import getMangaProgress, { setMangaProgress } from "../util/getMangaProgress";
 import getReading from "../util/getReading";
 import { getLists } from "../util/lists";
