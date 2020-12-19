694b7096416b6c6bda4033ec6e7da8f257b0c40f
Jip Fr <jipfrijlink@gmail.com>
2020-12-19 12:44:22 +0100

    Revert "Test"
    This reverts commit fe0b07878c4948b955646928c53db5ed056f9668.

--- src/web.ts
+++ src/web.ts
@@ -13,99 +13,99 @@ const app = express();
 
 // Set up view engine
 app.engine(
-    "handlebars",
-    handlebars({
-        helpers: {
-            stringify(v: Record<string, unknown>) {
-                return JSON.stringify(v);
-            },
-            getProgressString(manga: StoredData) {
-                if (manga.progress && manga.success) {
-                    const curChapter = manga.data.chapters.find(
-                        (c) => c.hrefString === manga.progress.chapterId
-                    );
-                    return (
-                        curChapter?.label +
-                            (typeof manga?.progress?.percentage !== "undefined"
-                                ? ` (${manga.progress.percentage}%)`
-                                : "") ?? "Chapter not found"
-                    );
-                }
-                return "Not started yet";
-            },
-            getScraperIcon(provider: string): string | null {
-                const icons = {
-                    mangasee: "https://mangasee123.com/media/favicon.png",
-                    mangadex: "https://mangadex.org/images/misc/navbar.svg",
-                    rco: "/provider/rco.png",
-                };
-                return icons[provider] ?? "/icons/main-on-white.png";
-            },
-            genLink2(
-                provider = "mangasee",
-                slug: string,
-                hrefString: string | null = null,
-                chapter = -1
-            ) {
-                const href = `/${getProviderId(provider)}/${slug}/`;
-                let seasonLink =
-                    typeof hrefString === "string" || typeof hrefString === "number"
-                        ? hrefString
-                        : "";
-                if (typeof chapter === "number" && chapter !== -1)
-                    seasonLink = `${hrefString}-${chapter}`; // Fallback stuff // TODO: Figure this out. This is related to reading at manga-card.handlebars, line 31
-                return `${href}${seasonLink}`;
-            },
-            isCurrentChapter(
-       season1: number,
-                season2: number,
-                chapter1: number,
-                chapter2: number
-            ) {
-                return season1 === season2 && chapter1 === chapter2
-                    ? "current-chapter badge-background"
-                    : "";
-            },
-            checkSmallHighlight(mangaSlug: string, currentPage: string) {
-                return mangaSlug === currentPage ? "currentManga badge-background" : "";
-            },
-            getChapterDate(chapter: Chapter) {
-                function pad(v: number, amount = 2) {
-                    return v.toString().padStart(amount, "0");
-                }
-                const d = new Date(chapter.date);
-                return `${pad(d.getDate())}-${pad(d.getMonth() + 1)}-${pad(
-                    d.getFullYear(),
-                    4
-                )}`;
-            },
-            getPageProgress(progress: Progress | void) {
-                if (!progress) return false;
-                return progress.percentage < 90 ? progress.current : 1;
-            },
-            ifDev(options) {
-                return process.env.dev ? options.fn(this) : options.inverse(this);
-            },
-            getChapterName(progress: Progress, manga: StoredData) {
-                if (!progress) return "shrug";
-                const current = manga.data.chapters.find(
-                    (v) => v.hrefString === progress.chapterId
-                );
-                return current ? current.label : "Unknown chapter";
-            },
-            ifNotByCreator(list: List, options) {
-                return list.byCreator ? options.inverse() : options.fn();
-            },
-            addOne(num: number) {
-                return num + 1;
-            },
-            getIconSrc,
-        },
-    })
+	"handlebars",
+	handlebars({
+		helpers: {
+			stringify(v: Record<string, unknown>) {
+				return JSON.stringify(v);
+			},
+			getProgressString(manga: StoredData) {
+				if (manga.progress && manga.success) {
+					const curChapter = manga.data.chapters.find(
+						(c) => c.hrefString === manga.progress.chapterId
+					);
+					return (
+						curChapter?.label +
+							(typeof manga?.progress?.percentage !== "undefined"
+								? ` (${manga.progress.percentage}%)`
+								: "") ?? "Chapter not found"
+					);
+				}
+				return "Not started yet";
+			},
+			getScraperIcon(provider: string): string | null {
+				const icons = {
+					mangasee: "https://mangasee123.com/media/favicon.png",
+					mangadex: "https://mangadex.org/images/misc/navbar.svg",
+					rco: "/provider/rco.png",
+				};
+				return icons[provider] ?? "/icons/main-on-white.png";
+			},
+			genLink2(
+				provider = "mangasee",
+				slug: string,
+				hrefString: string | null = null,
+				chapter = -1
+			) {
+				const href = `/${getProviderId(provider)}/${slug}/`;
+				let seasonLink =
+					typeof hrefString === "string" || typeof hrefString === "number"
+						? hrefString
+						: "";
+				if (typeof chapter === "number" && chapter !== -1)
+					seasonLink = `${hrefString}-${chapter}`; // Fallback stuff // TODO: Figure this out. This is related to reading at manga-card.handlebars, line 31
+				return `${href}${seasonLink}`;
+			},
+			isCurrentChapter(
+				season1: number,
+				season2: number,
+				chapter1: number,
+				chapter2: number
+			) {
+				return season1 === season2 && chapter1 === chapter2
+					? "current-chapter badge-background"
+					: "";
+			},
+			checkSmallHighlight(mangaSlug: string, currentPage: string) {
+				return mangaSlug === currentPage ? "currentManga badge-background" : "";
+			},
+			getChapterDate(chapter: Chapter) {
+				function pad(v: number, amount = 2) {
+					return v.toString().padStart(amount, "0");
+				}
+				const d = new Date(chapter.date);
+				return `${pad(d.getDate())}-${pad(d.getMonth() + 1)}-${pad(
+					d.getFullYear(),
+					4
+				)}`;
+			},
+			getPageProgress(progress: Progress | void) {
+				if (!progress) return false;
+				return progress.percentage < 90 ? progress.current : 1;
+			},
+			ifDev(options) {
+				return process.env.dev ? options.fn(this) : options.inverse(this);
+			},
+			getChapterName(progress: Progress, manga: StoredData) {
+				if (!progress) return "shrug";
+				const current = manga.data.chapters.find(
+					(v) => v.hrefString === progress.chapterId
+				);
+				return current ? current.label : "Unknown chapter";
+			},
+			ifNotByCreator(list: List, options) {
+				return list.byCreator ? options.inverse() : options.fn();
+			},
+			addOne(num: number) {
+				return num + 1;
+			},
+			getIconSrc,
+		},
+	})
 );
 app.set("view engine", "handlebars");
 app.set("view options", {
-    layout: "main",
+	layout: "main",
 });
 
 // Bodyparser
