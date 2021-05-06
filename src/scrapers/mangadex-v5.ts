e9c31f29151b7732aa184e77b03efe3430d7c6a3
Jip Fr <jipfrijlink@gmail.com>
2021-05-06 13:13:09 +0200

    feat(mangadex): error handling, more ogs

--- src/scrapers/mangadex-v5.ts
+++ src/scrapers/mangadex-v5.ts
@@ -30,11 +30,11 @@ export class mangahereClass extends Scraper {
 
 		if (query === "") {
 			// Get popular page
-			pageUrl = "https://api.mangadex.org/manga";
+			pageUrl = `https://api.mangadex.org/manga&limit=${resultCount}`;
 		} else {
 			pageUrl = `https://api.mangadex.org/manga?title=${encodeURIComponent(
 				query
-			)}`;
+			)}&limit=${resultCount}`;
 		}
 
 		// Fetch DOM for relevant page
@@ -42,9 +42,11 @@ export class mangahereClass extends Scraper {
 		const data = await pageReq.json();
 
 		// Get IDs from nodes
-		const ids = data.results.map((result) => {
-			return result.data.id;
-		});
+		const ids = data.results
+			.map((result) => {
+				return result.data.id;
+			})
+			.slice(0, resultCount);
 
 		// Get details for each search result
 		const searchResultData: ScraperResponse[] = await Promise.all(
@@ -90,118 +92,132 @@ export class mangahereClass extends Scraper {
 		slug: string,
 		chapterId: string
 	): Promise<ScraperResponse> {
-		// Get data
-		const dataReq = await fetch(`https://api.mangadex.org/manga/${slug}`);
-		let data = await dataReq.json();
-		data = data.data;
+		try {
+			// Get data
+			const dataReq = await fetch(`https://api.mangadex.org/manga/${slug}`);
+			let data = await dataReq.json();
+			data = data.data;
 
-		// Get title
-		const title =
-			data.attributes.title.en || Object.values(data.attributes.title)[0];
+			// Get title
+			const title =
+				data.attributes.title.en || Object.values(data.attributes.title)[0];
 
-		// Set temporary poster URL :/
-		let posterUrl = "https://i.imgur.com/6TrIues.jpg";
+			// Set temporary poster URL :/
+			let posterUrl = "https://i.imgur.com/6TrIues.jpg";
 
-		// Get genres from tags
-		const genres = data.attributes.tags.map((tag) => tag.attributes.name.en);
+			// Get genres from tags
+			const genres = data.attributes.tags.map((tag) => tag.attributes.name.en);
 
-		// Get alternate titles
-		const alternateTitles = data.attributes.altTitles.map((t) => t.en);
+			// Get alternate titles
+			const alternateTitles = data.attributes.altTitles.map((t) => t.en);
 
-		// Get status
-		const status = data.attributes.status.toLowerCase();
+			// Get status
+			const status = data.attributes.status.toLowerCase();
 
-		// Get chapters
-		let offset = 0;
-		let total = Infinity;
-		let allChapters = [];
+			// Get chapters
+			let offset = 0;
+			let total = Infinity;
+			let allChapters = [];
 
-		while (offset < total) {
-			// Cycle through pagination
-			const chapterReq = await fetch(
-				`https://api.mangadex.org/manga/${slug}/feed?offset=${offset}&limit=250&locales[]=en`
-			);
-			const chapterData = await chapterReq.json();
-			const mdChapters = chapterData.results
-				.map((v) => (v.result === "ok" ? v.data : null))
+			while (offset < total) {
+				// Cycle through pagination
+				const chapterReq = await fetch(
+					`https://api.mangadex.org/manga/${slug}/feed?offset=${offset}&limit=500&locales[]=en`
+				);
+				const chapterData = await chapterReq.json();
+				const mdChapters = chapterData.results
+					.map((v) => (v.result === "ok" ? v.data : null))
+					.filter(Boolean);
+
+				total = chapterData.total;
+				offset = chapterData.offset + chapterData.limit;
+
+				allChapters = [...allChapters, ...mdChapters];
+			}
+
+			// Map fetched chapters into Adolla format
+			const chapters: Chapter[] = allChapters
+				.map(
+					(ch): Chapter => {
+						const { attributes: a } = ch;
+						return {
+							label: `Vol ${a.volume}, chapter ${a.chapter}`,
+							chapter: a.chapter,
+							season: a.volume || 0,
+							date: a.publishAt,
+							hrefString: ch.id,
+							combined: a.volume * 1000 + a.chapter,
+						};
+					}
+				)
+				.sort((a, b) => a.combined - b.combined);
+
+			// Find images
+			let chapterImages = [];
+			if (chapterId != "-1") {
+				// Scrape page to find images
+				const chapter = allChapters.find((c) => c.id === chapterId);
+
+				if (chapter) {
+					const atHome = await (
+						await fetch(`https://api.mangadex.org/at-home/server/${chapter.id}`)
+					).json();
+					const { baseUrl } = atHome;
+					chapterImages = chapter.attributes.data.map(
+						(id) => `${baseUrl}/data/${chapter.attributes.hash}/${id}`
+					);
+				} else {
+					console.error(
+						chalk.red("[Mangadex]") +
+							` A MangaDex chapter was requested but chapter was not found`
+					);
+				}
+			}
+
+			// Find description
+			const descriptionParagraphs = Entities.decode(
+				data.attributes.description.en
+			)
+				.replace(/\r/g, "")
+				.split("\n")
 				.filter(Boolean);
 
-			total = chapterData.total;
-			offset = chapterData.offset + chapterData.limit;
+			// See if manga is NSFW
+			const nsfw = data.attributes.contentRating !== "safe";
 
-			allChapters = [...allChapters, ...mdChapters];
+			// Return it.
+			const providerId = getProviderId(this.provider);
+			console.info(
+				chalk.green("[Mangadex]") +
+					` Successfully fetched: ${slug}, ${chapterId}`
+			);
+
+			return {
+				constant: {
+					title,
+					slug,
+					posterUrl,
+					alternateTitles,
+					descriptionParagraphs,
+					genres,
+					nsfw,
+				},
+				data: {
+					chapters,
+					chapterImages,
+					status,
+				},
+				success: true,
+				provider: isProviderId(providerId) ? providerId : null,
+			};
+		} catch (err) {
+			// OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!
+			// console.error(err.stack);
+			console.error(
+				chalk.red("[Mangadex]") + ` Failed to fetch: ${slug}, ${chapterId}`
+			);
+			return error(-1, err);
 		}
-
-		// Map fetched chapters into Adolla format
-		const chapters: Chapter[] = allChapters
-			.map(
-				(ch): Chapter => {
-					const { attributes: a } = ch;
-					return {
-						label: `Vol ${a.volume}, chapter ${a.chapter}`,
-						chapter: a.chapter,
-						season: a.volume || 0,
-						date: a.publishAt,
-						hrefString: ch.id,
-						combined: a.volume * 1000 + a.chapter,
-					};
-				}
-			)
-			.sort((a, b) => a.combined - b.combined);
-
-		// Find images
-		let chapterImages = [];
-		if (chapterId != "-1") {
-			// Scrape page to find images
-			const chapter = allChapters.find((c) => c.id === chapterId);
-
-			if (chapter) {
-				const atHome = await (
-					await fetch(`https://api.mangadex.org/at-home/server/${chapter.id}`)
-				).json();
-				const { baseUrl } = atHome;
-				chapterImages = chapter.attributes.data.map(
-					(id) => `${baseUrl}/data/${chapter.attributes.hash}/${id}`
-				);
-			} else {
-				console.error(
-					chalk.red("[Mangadex]") +
-						` A MangaDex chapter was requested but chapter was not found`
-				);
-			}
-		}
-
-		// Find description
-		const descriptionParagraphs = Entities.decode(
-			data.attributes.description.en
-		)
-			.replace(/\r/g, "")
-			.split("\n")
-			.filter(Boolean);
-
-		// See if manga is NSFW
-		const nsfw = data.attributes.contentRating !== "safe";
-
-		// Return it.
-		const providerId = getProviderId(this.provider);
-		return {
-			constant: {
-				title,
-				slug,
-				posterUrl,
-				alternateTitles,
-				descriptionParagraphs,
-				genres,
-				nsfw,
-			},
-			data: {
-				chapters,
-				chapterImages,
-				status,
-			},
-			success: true,
-			provider: isProviderId(providerId) ? providerId : null,
-		};
 	}
 }
 
