c6a20d5c744d39a75d89cb0ea8361536837c746f
Jip Fr <jipfrijlink@gmail.com>
2021-07-11 20:17:58 +0200

    fix(secret-config): fix secret config deprecation message

--- src/util/secretConfig.ts
+++ src/util/secretConfig.ts
@@ -3,7 +3,6 @@ import path from "path";
 import chalk from "chalk";
 import fs from "fs";
 import exampleConfig from "../example.secret-config.json";
-import { formatDiagnostic } from "typescript";
 
 interface SecretConfig {
 	telegram?: {
@@ -22,22 +21,33 @@ interface SecretConfig {
 let secretConfig: SecretConfig | null;
 
 const configPath = path.join(os.homedir(), ".adolla", "secret-config.json");
+const oldPath =
+	__dirname.split("/").slice(0, -1).join("/") + "/secret-config.json"; // Ew
+console.log(oldPath);
 
 if (!fs.existsSync(configPath)) {
 	// Set default secret config
 	let newConfig;
 	// Find new config to set. Either the existing one inside this directory or the example file
-	const oldPath =
-		__dirname.split("/").slice(0, -1).join("/") + "/secret-config.json"; // Ew
 
 	if (fs.existsSync(oldPath)) {
+		console.error(
+			chalk.red("[SECRET]") +
+				` Found secret-config.json in the \`src\` folder. This location is deprecated and is moved to \`~/.adolla/secret-config.json\`.`
+		);
 		newConfig = fs.readFileSync(oldPath, "utf-8");
-		fs.renameSync(oldPath, "secret-config-archived.json");
+		fs.renameSync(oldPath, "src/secret-config-archived.json");
 	} else {
 		newConfig = JSON.stringify(exampleConfig);
 	}
 
 	fs.writeFileSync(configPath, newConfig);
+} else if (fs.existsSync(oldPath)) {
+	console.error(
+		chalk.red("[SECRET]") +
+			` Found secret-config.json in the \`src\` folder. This location is deprecated and is moved to \`~/.adolla/secret-config.json\`.`
+	);
+	fs.renameSync(oldPath, "secret-config-archived.json");
 }
 
 if (fs.existsSync(configPath)) {
