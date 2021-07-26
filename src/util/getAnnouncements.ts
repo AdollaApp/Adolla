27cd7bb385310d51441c4c92c46d6d34b277a576
Jip Fr <jipfrijlink@gmail.com>
2021-07-26 21:29:53 +0200

    refactor(announcements): try/catch fetching of announcements

--- src/util/getAnnouncements.ts
+++ src/util/getAnnouncements.ts
@@ -25,82 +25,94 @@ let data: { at: number; data: Announcement[] } = {
 };
 
 export async function getAnnouncements(): Promise<Announcement[]> {
-	// See if announcement cache is valid or not
-	const diff = Date.now() - data.at;
-	if (diff > 1e3 * 60 * 30) {
-		data.data = await (await fetch(gistUrl)).json();
-		data.at = Date.now();
-	}
-
-	const notifiedAnnouncements = db.get("other.announcements-sent") || [];
-	const dismissedAnnouncements = db.get("other.announcements-dismissed") || [];
-
-	for (const announcement of data.data) {
-		const d = new Date(announcement.date);
-		const fmtDate = `${months[d.getMonth()]} ${d.getDate()} ${d.getFullYear()}`;
-		announcement.readableDate = fmtDate;
-
-		// Notify people
-		if (!notifiedAnnouncements.includes(announcement.id)) {
-			console.info(
-				chalk.green("[NOTIFS]") +
-					` New announcement. Attempting methods of sending out notifications.`
-			);
-
-			const bot = Bot.get();
-			if (bot) {
-				// Send notification, and do some stuff to make sure it doesn't send it every 30 minutes
-				console.info(
-					chalk.green("[NOTIFS]") +
-						` New announcement: ${announcement.message}, notifying user with Telegram bot`
-				);
-
-				Bot.send(`**Word has come from The Creator:** ${announcement.message}`);
-			}
-
-			// Discord webhook
-			if (secretConfig.discord_webhook) {
-				const discordReq = await fetch(secretConfig.discord_webhook, {
-					method: "POST",
-					headers: {
-						"content-type": "application/json",
-					},
-					body: JSON.stringify({
-						avatar_url:
-							"https://raw.githubusercontent.com/AdollaApp/Adolla/master/public/icons/white-on-blue.png",
-						username: "Adolla",
-						embeds: [
-							{
-								title: "The Creator has spoken:",
-								description: announcement.message,
-								color: 4959182,
-								author: {
-									name: "Adolla",
-									url: "https://jipfr.nl/adolla",
-									icon_url:
-										"https://raw.githubusercontent.com/AdollaApp/Adolla/master/public/icons/white-on-blue.png",
-								},
-							},
-						],
-					}),
-				});
-
-				console.info(
-					chalk.green("[NOTIFS]") +
-						` New announcement: ${announcement.message}, attempted to notify user over Discord Webhook. HTTP status ${discordReq.status}`
-				);
-			}
-
-			notifiedAnnouncements.push(announcement.id);
+	try {
+		// See if announcement cache is valid or not
+		const diff = Date.now() - data.at;
+		if (diff > 1e3 * 60 * 30) {
+			data.data = await (await fetch(gistUrl)).json();
+			data.at = Date.now();
 		}
+
+		const notifiedAnnouncements = db.get("other.announcements-sent") || [];
+		const dismissedAnnouncements =
+			db.get("other.announcements-dismissed") || [];
+
+		for (const announcement of data.data) {
+			const d = new Date(announcement.date);
+			const fmtDate = `${
+				months[d.getMonth()]
+			} ${d.getDate()} ${d.getFullYear()}`;
+			announcement.readableDate = fmtDate;
+
+			// Notify people
+			if (!notifiedAnnouncements.includes(announcement.id)) {
+				console.info(
+					chalk.green("[NOTIFS]") +
+						` New announcement. Attempting methods of sending out notifications.`
+				);
+
+				const bot = Bot.get();
+				if (bot) {
+					// Send notification, and do some stuff to make sure it doesn't send it every 30 minutes
+					console.info(
+						chalk.green("[NOTIFS]") +
+							` New announcement: ${announcement.message}, notifying user with Telegram bot`
+					);
+
+					Bot.send(
+						`**Word has come from The Creator:** ${announcement.message}`
+					);
+				}
+
+				// Discord webhook
+				if (secretConfig.discord_webhook) {
+					const discordReq = await fetch(secretConfig.discord_webhook, {
+						method: "POST",
+						headers: {
+							"content-type": "application/json",
+						},
+						body: JSON.stringify({
+							avatar_url:
+								"https://raw.githubusercontent.com/AdollaApp/Adolla/master/public/icons/white-on-blue.png",
+							username: "Adolla",
+							embeds: [
+								{
+									title: "The Creator has spoken:",
+									description: announcement.message,
+									color: 4959182,
+									author: {
+										name: "Adolla",
+										url: "https://jipfr.nl/adolla",
+										icon_url:
+											"https://raw.githubusercontent.com/AdollaApp/Adolla/master/public/icons/white-on-blue.png",
+									},
+								},
+							],
+						}),
+					});
+
+					console.info(
+						chalk.green("[NOTIFS]") +
+							` New announcement: ${announcement.message}, attempted to notify user over Discord Webhook. HTTP status ${discordReq.status}`
+					);
+				}
+
+				notifiedAnnouncements.push(announcement.id);
+			}
+		}
+
+		db.set("other.announcements-sent", notifiedAnnouncements);
+
+		// Unread announcements
+		const unreadAnnouncements = data.data.filter((announcement) => {
+			return !dismissedAnnouncements.includes(announcement.id);
+		});
+
+		return unreadAnnouncements;
+	} catch (err) {
+		console.error(
+			chalk.red("[Announcements]") + ` Unable to fetch announcements: ${err}`
+		);
+		return [];
 	}
-
-	db.set("other.announcements-sent", notifiedAnnouncements);
-
-	// Unread announcements
-	const unreadAnnouncements = data.data.filter((announcement) => {
-		return !dismissedAnnouncements.includes(announcement.id);
-	});
-
-	return unreadAnnouncements;
 }
