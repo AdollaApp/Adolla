bf6d3108922bc737e8f40e08252e17880fb25a87
Jip Fr <jipfrijlink@gmail.com>
2023-02-17 00:29:24 +0100

    Oops

--- public/js_compiled/service-worker-push.js
+++ public/js_compiled/service-worker-push.js
@@ -3,10 +3,10 @@
 //Event that shows a notification when is received by push
 self.addEventListener("push", function (event) {
   var data = event.data.json();
-  console.log(data, typeof (data === null || data === void 0 ? void 0 : data.title) === "string");
   if (typeof (data === null || data === void 0 ? void 0 : data.title) === "string") {
-    console.log("Sending");
-    self.registration.showNotification(data.title).then(console.log);
+    self.registration.showNotification(data.title, {
+      body: data.body
+    }).then(console.log);
   }
   if ("setAppBadge" in navigator && typeof data.badgeCount === "number") {
     var _data$badgeCount;
