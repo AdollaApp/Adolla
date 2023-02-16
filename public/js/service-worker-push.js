0bbc92c81fc0bf909a81467c693588a040b674f2
Jip Fr <jipfrijlink@gmail.com>
2023-02-17 00:19:41 +0100

    Re-add body

--- public/js/service-worker-push.js
+++ public/js/service-worker-push.js
@@ -1,10 +1,12 @@
 //Event that shows a notification when is received by push
 self.addEventListener("push", (event) => {
 	const data = event.data.json();
-	console.log(data, typeof data?.title === "string");
 	if (typeof data?.title === "string") {
-		console.log("Sending");
-		self.registration.showNotification(data.title).then(console.log);
+		self.registration
+			.showNotification(data.title, {
+				body: data.body,
+			})
+			.then(console.log);
 	}
 	if ("setAppBadge" in navigator && typeof data.badgeCount === "number") {
 		navigator.setAppBadge(data.badgeCount ?? 2);
