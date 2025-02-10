e0210bbe9251d9c18c8da8819bc696db7034d257
mrjvs <mistrjvs@gmail.com>
2025-02-10 19:27:17 +0100

    Add API project

---
+++ api/src/main.ts
@@ -0,0 +1,18 @@
+// Import the framework and instantiate it
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  Deno.exit(1);
+}
