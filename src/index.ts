bf392a850177a349fb1cb3e0ca0253675493cc73
Jip Fr <jipfrijlink@gmail.com>
2025-02-19 15:34:55 +0100

    Woop

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ import secretConfig from "./util/secretConfig";
 		// Generate unique ID
 		if (!db.get("adolla-uid")) {
 			const words = await fetch(
-				"https://raw.githubusercontent.com/adollaapp/adolla/master/small-words.json"
+				"https://raw.githubusercontent.com/AdollaApp/Adolla/refs/heads/master/small-words.json"
 			).then((d) => d.json());
 
 			let uidArr = [];
