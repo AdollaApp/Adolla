60350b7afad1d4b160b26e3d471605d81b14cbef
Jip Fr <jipfrijlink@gmail.com>
2025-02-23 16:05:09 +0100

    chore: don't proxy images (oops!)

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   import MangaGrid from "$lib/components/manga/Grid.svelte";
   import MangaCard from "$lib/components/manga/MangaCard.svelte";
-  import Navbar from "$lib/components/nav/Navbar.svelte";
   import LargeHeading from "$lib/components/text/LargeHeading.svelte";
   import Container from "$lib/components/util/Container.svelte";
 </script>
@@ -11,7 +10,7 @@
   <MangaGrid>
     <MangaCard
       title="Karakai Jouzu no (Moto) Takagi-san"
-      image="https://adolla.frij.link/proxy-image/?url=https://uploads.mangadex.org/covers/ddcebfa2-05a0-4fcd-9cd4-60d779c38f2c/d1ae5186-f108-4983-ac17-b2dbe6a38ae3.jpg.512.jpg"
+      image="https://uploads.mangadex.org/covers/ddcebfa2-05a0-4fcd-9cd4-60d779c38f2c/d1ae5186-f108-4983-ac17-b2dbe6a38ae3.jpg.512.jpg"
       isNew={true}
       chapterName="Chapter 314.25"
       isNsfw={false}
@@ -20,7 +19,7 @@
     {#each new Array(10) as _}
       <MangaCard
         title="Telework Yotabanashi"
-        image="https://adolla.frij.link/proxy-image/?url=https://uploads.mangadex.org/covers/d09c8abd-24ec-41de-ac8b-b6381a2f3a63/66c7de64-3449-4434-bd87-60a7c3400a1f.jpg.512.jpg"
+        image="https://uploads.mangadex.org/covers/d09c8abd-24ec-41de-ac8b-b6381a2f3a63/66c7de64-3449-4434-bd87-60a7c3400a1f.jpg.512.jpg"
         isNew={false}
         chapterName="Vol 1, chapter 8"
         isNsfw={false}
